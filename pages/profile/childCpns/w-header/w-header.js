84846d530d345eadae95d6eb881cbe646eec0a4a
jiangchao <414345390@qq.com>
2020-04-08 19:13:32 +0800

    完成我的信息

---
+++ pages/profile/childCpns/w-header/w-header.js
@@ -0,0 +1,25 @@
+// pages/profile/childCpns/w-header/w-header.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    userInfo:{
+      type:Object,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,17 +1,62 @@
 // pages/profile/profile.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    orderList: [{ icon: 'message.png', info: '我的消息' },
+                { icon: 'pointer.png', info: '积分商城' },
+                { icon: 'vip.png', info: '会员卡' },  ],
+                
+    serviceList: [{ icon: 'cart.png', info: '我的购物车' },
+                  { icon: 'app.png', info: '下载购物APP' },],
+    userInfo:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        console.log(res);
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              console.log(res);
+              console.log("---------------->");
+              // 可以将 res 发送给后台解码出 unionId
+              app.globalData.userInfo = res.userInfo;
+              const userInfos = res.userInfo;
+              this.setData({
+                userInfo: userInfos
+              })
+              console.log(this.data.userInfo);
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (app.userInfoReadyCallback) {
+                app.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
 
   },
 
